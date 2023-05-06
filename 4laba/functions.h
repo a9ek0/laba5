660eb864b59c14a67437a42df241aade73f34bc0
a9ek0 <109074282+a9ek0@users.noreply.github.com>
2023-05-06 20:30:30 +0300

    Delete 4laba directory

--- 4laba/functions.h
+++
@@ -1,8 +0,0 @@
-#ifndef INC_4LABA_FUNCTIONS_H
-#define INC_4LABA_FUNCTIONS_H
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#include <time.h>
-#define MAX_LINE_LENGTH 1024
-#endif //INC_4LABA_FUNCTIONS_H
