387dc61cc2e61ad78e8f3df2ca1cd992dc5e2315
a9ek0 <109074282+a9ek0@users.noreply.github.com>
2023-05-06 20:29:55 +0300

    Delete 1laba directory

--- 1laba/mylib/check.h
+++
@@ -1,9 +0,0 @@
-//
-// Created by asduw on 06.03.2023.
-//
-#include <stdio.h>
-#include <wchar.h>
-#ifndef INC_1LABA_CHECK_H
-#define INC_1LABA_CHECK_H
-void check_in_range(int *value, int left_boarder, int right_boarder);
-#endif //INC_1LABA_CHECK_H
